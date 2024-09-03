97cc936ebc2714ca0b12429fe1c64ca63f771712
Max Frischknecht <info@maxfrischknecht.ch>
2024-09-03 15:14:25 +0200

    initialize svelte app, create header

---
+++ src/lib/header.svelte
@@ -0,0 +1,38 @@
+<script>
+	$: pageTitle = 'Generated Visualization';
+</script>
+
+<header>
+	<!-- HEADER BAR -->
+	<div class="grid grid-cols-12 gap-6">
+		<div class="col-span-10 col-start-2 bg-black px-6 pb-1 h-12 flex items-center border-l border-r border-grey">
+			<h1>Collection Ernst Brunner</h1>
+			<h2 class="pl-4 text-blue">{pageTitle}</h2>
+		</div>
+	</div>
+	<!-- VISUALIZATION GENERATOR -->
+	<div class="grid grid-cols-12 gap-6">
+		<div class="col-span-10 col-start-2 border border-grey">
+			<div class="grid grid-cols-3 px-6 gap-6 bg-black">
+				<div class="col-span-1 py-5 border-r border-grey">
+					<p class="font-mono text-mono-sm pb-3">Analytical Interest</p>
+					<p>Releationship</p>
+                    <p>Time</p>
+                    <p>Space</p>
+				</div>
+				<div class="col-span-1 pt-5 pb-4 border-r border-grey">
+					<p class="font-mono text-mono-sm pb-3">Data</p>
+					<p>Keywords</p>
+					<p>Dates</p>
+					<p>Locations</p>
+					<p>Comments</p>
+				</div>
+                <div class="col-span-1 pt-5 pb-4 ">
+					<p class="font-mono text-mono-sm pb-3">Data Manipulation</p>
+					<p>Co-Occurence Matrix</p>
+					<p>Force-Directed Graph</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+				grey: '#5A5A5A',
+				dark: '#282828',
+				light: '#F4F4F4',
+				background: '#141414',
+				blue: '#8390FA',
+        black: "#000"
+			}
+    },
+    fontFamily: {
+			// creates font-sans class
+			sans: ['Helvetica Neue', 'sans-serif'],
+			mono: ['Everett Mono', 'monospace']
+		},
+		fontSize: {
+			// https://tailwindcss.com/docs/customizing-spacing#default-spacing-scale
+      base: ['1.125rem', '1.375rem'],
+			'sans-md': ['1.125rem', '1.375rem'],
+			'mono-sm': ['0.71875rem', '1rem'],
+		}
+  },
+  plugins: [],
+}
+
