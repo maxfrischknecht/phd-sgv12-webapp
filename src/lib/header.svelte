18789e0906896716eb64d4dfcea067a31bfb398d
Max Frischknecht <info@maxfrischknecht.ch>
2024-09-06 15:06:26 +0200

    build navigation

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,34 +1,80 @@
 <script>
 	import { onMount } from 'svelte';
-	let settings = null;
-	let activeMetaData = null;
+	import Tread from './tread.svelte';
 
+	// get the current route to set the settings on reload
+	// /keywords, /co-occurence-matrix
+	import { page } from '$app/stores';
+	$: firstLevelRoute = $page.url.pathname.split('/')[1]; // keywords
+	$: secondLevelRoute = $page.url.pathname.split('/')[2]; // co-occurence
+	$: thirdLevelRoute = $page.url.pathname.split('/')[3]; // matrix
+
+	// keep track of the current settings based on section interaction
+	$: currentSetting = {
+		data: null,
+		'meta-data': null,
+		'data-curation': null,
+		'data-visualization': null
+	};;
+
+	// the settings to create all navigation options
+	let settings = null;
 	// Fetch the settings to dynamically generate the navigations
 	onMount(async () => {
 		try {
 			const response = await fetch('/generative-settings.json');
 			if (response.ok) {
 				settings = await response.json();
-				console.log('Data fetched:', settings);
+				// set data set to default
+				currentSetting['data'] = settings['data-set'][0];
 			} else {
 				console.error('Failed to load JSON data');
 			}
 		} catch (error) {
 			console.error('Error fetching data:', error);
 		}
+
+		// on mount set all settings based on current routes
+		if (firstLevelRoute) {
+			let currentMetaData = settings['meta-data'].find((item) => item.id === firstLevelRoute);
+			if (currentMetaData) currentSetting['meta-data'] = currentMetaData;
+		}
+		if (secondLevelRoute) {
+			let currentDataCuration = settings['data-curation'][firstLevelRoute].find((item) => item.id === secondLevelRoute)
+			if (currentDataCuration) currentSetting["data-curation"] = currentDataCuration;
+		}
+		if (thirdLevelRoute){
+			let currentDataVisualization = settings['data-visualization'][secondLevelRoute].find((item) => item.id === thirdLevelRoute)
+			if (currentDataVisualization) currentSetting["data-visualization"] = currentDataVisualization;
+		}
 	});
 
-	// Get the current page for dynamic subnavigation
-	// Get the current route an format it from "/about" to "About"
-	// save it in a reactive variable with $:
-	import { page } from '$app/stores';
-	$: currentPage = $page.url.pathname.split('/')[1];
-
-	// console.log($page, currentPage);
+	// update the currentSettings by section interaction
+	function setDataSet() {
+		currentSetting['meta-data'] = null;
+		currentSetting['data-curation'] = null;
+		currentSetting['data-visualization'] = null;
+		// console.log('set data set', 'default ernst brunner negatives');
+	}
+	function setMetaData(metadata) {
+		currentSetting['meta-data'] = metadata;
+		currentSetting['data-curation'] = null;
+		currentSetting['data-visualization'] = null;
+		// console.log('set meta data', metadata);
+	}
+	function setDataCuration(datacuration) {
+		currentSetting['data-curation'] = datacuration;
+		currentSetting['data-visualization'] = null;
+		// console.log('set data curation', datacuration);
+	}
+	function setDataVisualization(datavisualization) {
+		currentSetting['data-visualization'] = datavisualization;
+		// console.log('set data curation', datavisualization);
+	}
 </script>
 
 <header class="w-100 min-h-8 bg-background">
-	<!-- SECTIONS HEADERS -->
+	<!-- 1st Row: SETTINGS BTN & SECTIONS HEADERS -->
 	<div class="grid grid-cols-12 gap-6 px-6">
 		<button class="col-span-2 bg-light text-dark font-mono text-mono-sm h-8">Settings</button>
 		{#if settings === null}
@@ -42,35 +88,75 @@
 		{/if}
 	</div>
 
-	<!-- SECTION OPTIONS -->
+	<!-- 2nd Row: SECTION OPTIONS -->
 	<div class="grid grid-cols-12 gap-6 px-6 pb-4 border-b border-grey">
-		<!-- DATA SET OPTIONS -->
+		<!-- DATA SET OPTIONS aka HOME BTN -->
+		<!-- always visible -->
 		{#if settings != null && settings['data-set'] && settings['data-set'].length > 0}
 			<div class="col-start-3 col-span-2 font-sans text-sans-md text-blue">
-				<p>{settings['data-set'][0]['label']}</p>
+				<a on:click={setDataSet} href="/">{settings['data-set'][0]['label']}</a>
 			</div>
 		{/if}
 		<!-- META DATA OPTIONS -->
+		<!-- always visible -->
 		{#if settings != null && settings['meta-data'] && settings['meta-data'].length > 0}
 			<div class="col-span-2 font-sans text-sans-md">
 				{#each settings['meta-data'] as option}
 					<a
-						on:mouseover={() => (activeMetaData = option.url)}
-						on:focus={() => (activeMetaData = option.url)}
-						href="/{option.url}"
-						class="block">{option.label}</a
-					>
+						on:click={setMetaData(option)}
+						on:mouseover={setMetaData(option)}
+						on:focus={setMetaData(option)}
+						href="/{option.id}"
+						class="block {currentSetting['meta-data'] &&
+						currentSetting['meta-data']['id'] === option.id
+							? 'text-blue'
+							: ''}"
+						>{option.label}
+					</a>
 				{/each}
 			</div>
 		{/if}
 		<!-- DATA CURATION OPTIONS -->
-		{#if settings != null && currentPage}
+		<!-- visible depending on subpage route, e.g /keywords -->
+		<!-- not clickable, only shows viz options -->
+		{#if settings != null && currentSetting['meta-data']}
 			<div class="col-span-2 font-sans text-sans-md">
-				{#each Object.keys(settings['data-curation']) as sectionKey}
-					{#if activeMetaData == sectionKey}
-						<div id={sectionKey}>
-							{#each settings['data-curation'][sectionKey] as option}
-								<a href="/{option.url}" class="block">{option.label}</a>
+				{#each Object.keys(settings['data-curation']) as optionKey}
+					{#if currentSetting['meta-data']['id'] == optionKey}
+						<div id={optionKey}>
+							{#each settings['data-curation'][optionKey] as option}
+								<!-- hover over these will dynamically show data viz options -->
+								<button
+									on:mouseover={setDataCuration(option)}
+									on:focus={setDataCuration(option)}
+									class="block {currentSetting['data-curation'] &&
+									currentSetting['data-curation']['id'] === option.id
+										? 'text-blue'
+										: ''}">{option.label}</button
+								>
+							{/each}
+						</div>
+					{/if}
+				{/each}
+			</div>
+		{/if}
+		<!-- DATA VISUALIZATION OPTIONS -->
+		<!-- visible on hover of data curation option -->
+		{#if settings != null && currentSetting['data-curation'] != null}
+			<div class="col-span-2 font-sans text-sans-md">
+				{#each Object.keys(settings['data-visualization']) as optionKey}
+					{#if currentSetting['data-curation']['id'] == optionKey}
+						<div id={optionKey}>
+							{#each settings['data-visualization'][optionKey] as option}
+								<a
+									on:mouseover={setDataVisualization(option)}
+									on:focus={setDataVisualization(option)}
+									href="/{firstLevelRoute}/{optionKey}/{option.id}"
+									class="block {currentSetting['data-visualization'] &&
+									currentSetting['data-visualization']['id'] === option.id
+										? 'text-blue'
+										: ''}">{option.label}</a
+								>
 							{/each}
 						</div>
 					{/if}
@@ -78,4 +164,7 @@
 			</div>
 		{/if}
 	</div>
+
+	<!-- THREAD -->
+	<Tread {currentSetting} />
 </header>
---
+++ src/lib/tread.svelte
@@ -0,0 +1,32 @@
+<script>
+	export let currentSetting;
+	console.log('thread', currentSetting);
+</script>
+
+<div class="grid grid-cols-12 gap-6 px-6 border-b border-grey pb-4">
+	<button class="col-span-2 bg-light text-dark font-mono text-mono-sm h-8">Thread</button>
+	<!-- DATA SETDETAILS -->
+	<div class="col-span-2 mt-2">
+		{#if currentSetting && currentSetting['data']}
+			<p class="font-mono text-mono-sm">{currentSetting['data']['details']}</p>
+		{/if}
+	</div>
+	<!-- METADATA DETAILS -->
+	<div class="col-span-2 mt-2">
+		{#if currentSetting && currentSetting['meta-data']}
+			<p class="font-mono text-mono-sm">{currentSetting['meta-data']['details']}</p>
+		{/if}
+	</div>
+	<!-- DATA CURATION DETAILS -->
+	<div class="col-span-2 mt-2">
+		{#if currentSetting && currentSetting['data-curation']}
+			<p class="font-mono text-mono-sm">{currentSetting['data-curation']['details']}</p>
+		{/if}
+	</div>
+	<!-- DATA VISUALIZATION DETAILS -->
+	<div class="col-span-2 mt-2">
+		{#if currentSetting && currentSetting['data-visualization']}
+			<p class="font-mono text-mono-sm">{currentSetting['data-visualization']['details']}</p>
+		{/if}
+	</div>
+</div>
